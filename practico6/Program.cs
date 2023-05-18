2419acff42bce4e93c5dc6623ce80adf71eecb23
Alvaro Diaz <alvaroad29012001@gmail.com>
2023-05-18 19:00:46 -0300

    ejercicio 1

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,14 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-int a;
+//int a;
 
-int b;
+//int b;
 
-a = 10;
+//a = 10;
 
-b = a;
+//b = a;
 
-Console.WriteLine("Valor de a:"+a);
+//Console.WriteLine("Valor de a:"+a);
 
-Console.WriteLine("Valor de b:"+b);
+//Console.WriteLine("Valor de b:"+b);
+
+//ejercicio 1
+int numero;
+Console.WriteLine("Ingrese un numero");
+string numeroString = Console.ReadLine(); //leo el numero
+while (!int.TryParse(numeroString,out numero)) //si es un numero devuelve true y guarda el numero en "numero",si no es un numero devuelve false
+{
+    Console.WriteLine("Numero ingresado no valido");
+    Console.WriteLine("Ingrese un numero");
+    numeroString = Console.ReadLine();    
+}
+
+int resto = 0,divisor = numero,i=10,invertido = 0;
+
+do
+{
+    resto = divisor % 10;
+    divisor = divisor / 10;
+    invertido = invertido * i + resto;
+} while (divisor > 0);
+
+Console.WriteLine("Numero ingresado:"+numero);
+Console.WriteLine("Numero invertido:"+invertido);
