e0998597a0f2a0d717610e91f6d5c541951e6913
Alvaro Diaz <alvaroad29012001@gmail.com>
2023-05-19 11:12:44 -0300

    Ejercicio 2, rama: CalculadoraV1

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,37 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-//int a;
-
-//int b;
-
-//a = 10;
-
-//b = a;
-
-//Console.WriteLine("Valor de a:"+a);
-
-//Console.WriteLine("Valor de b:"+b);
-
-//ejercicio 1
-int numero;
-Console.WriteLine("Ingrese un numero");
-string numeroString = Console.ReadLine(); //leo el numero
-while (!int.TryParse(numeroString,out numero)) //si es un numero devuelve true y guarda el numero en "numero",si no es un numero devuelve false
-{
-    Console.WriteLine("Numero ingresado no valido");
-    Console.WriteLine("Ingrese un numero");
-    numeroString = Console.ReadLine();    
-}
-
-int resto = 0,divisor = numero,i=10,invertido = 0;
+﻿//ejercicio 2
+float num1,num2;
+int op = 1;
 
 do
 {
-    resto = divisor % 10;
-    divisor = divisor / 10;
-    invertido = invertido * i + resto;
-} while (divisor > 0);
+    do
+    {
+        Console.WriteLine("<><><><><><><><><><><><><><><><><><><><><><><>");
+        Console.WriteLine("<>   Ingrese la operacion a realizar:       <>");
+        Console.WriteLine("<>   0-Salir                                <>");
+        Console.WriteLine("<>   1-Suma                                 <>");
+        Console.WriteLine("<>   2-Resta                                <>");
+        Console.WriteLine("<>   3-Multiplicacion                       <>");
+        Console.WriteLine("<>   4-Division                             <>");
+        Console.WriteLine("<><><><><><><><><><><><><><><><><><><><><><><>");
+    } while (!int.TryParse(Console.ReadLine(),out op) || op < 0 || op > 4); //si ingreso un texto o un valor que no este en el rango seguir iterando
 
-Console.WriteLine("Numero ingresado:"+numero);
-Console.WriteLine("Numero invertido:"+invertido);
+    if (op != 0)
+    {
+        do
+        {
+            Console.WriteLine("<<<< Ingrese el primer numero >>>>");  
+        } while (!float.TryParse(Console.ReadLine(),out num1));
+
+        do
+        {
+            Console.WriteLine("<<<< Ingrese el segundo numero >>>>"); 
+        } while (!float.TryParse(Console.ReadLine(),out num2));
+
+        switch (op)
+        {
+            case 1:
+                Console.WriteLine("\nSuma : " + (num1+num2)+"\n");
+                break;
+
+            case 2:
+                Console.WriteLine($"\nResta de {num1} y {num2} es {num1-num2}\n");
+                break;
+
+            case 3:
+                Console.WriteLine($"\nProducto de {num1} y {num2} es {num1*num2}\n");
+                break;
+
+            case 4:
+                Console.WriteLine($"\nDivision de {num1} entre {num2} es {(num1/num2).ToString("N2")}\n");
+                break;
+        }
+
+        do
+        {
+            Console.WriteLine("<<<< Desea continuar? SI[1] / NO[0] >>>>");
+        } while (!int.TryParse(Console.ReadLine(),out op) || op < 0 || op > 1);
+    }
+} while (op != 0);
